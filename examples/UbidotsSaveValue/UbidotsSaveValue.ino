790159ba349872e1c8f54b0c4d01f7fd27211647
gamo256 <gamo256@gmail.com>
2016-04-24 03:16:49 +0900

    Update UbidotsSaveValue.ino

--- examples/UbidotsSaveValue/UbidotsSaveValue.ino
+++ examples/UbidotsSaveValue/UbidotsSaveValue.ino
@@ -7,15 +7,19 @@
 #define TOKEN "Your_token_here"
 #define ID "Your_VariableID_here"
 
+// Create a Client object
 Ubidots client(TOKEN);
 
 void setup() {
   Serial.begin(9600);
+  // Connect Wifi
   client.wifiConnection(SSID,PASS);
+  // Open TCP connection
   client.tcpConnection();
 }
 
 void loop() {
   float value = analogRead(A0);
+  // Send one value
   client.saveValue(ID,value);
 }
