b295184a24179daa8b60cb467f21b4b7603f6119
gamo256 <gamo256@gmail.com>
2016-04-24 03:14:45 +0900

    Update UbidotsSaveMultiValues.ino

--- examples/UbidotsSaveMultiValues/UbidotsSaveMultiValues.ino
+++ examples/UbidotsSaveMultiValues/UbidotsSaveMultiValues.ino
@@ -1,10 +1,8 @@
 #include <UbidotsESP8266.h>
 #include <SoftwareSerial.h> 
 
-
 #define SSID "Your_WiFi_SSID_here"
 #define PASS "Your_WiFi_PASWORD_here"
-
 
 #define TOKEN "Your_token_here"
 #define ID "Your_id_here" // Replace it with your Ubidots' variable ID
@@ -12,23 +10,27 @@
 #define ID3 "Your_id3_here"
 #define ID4 "Your_id4_here" 
 
+// Create a Client object
 Ubidots client(TOKEN);
 
 void setup() {
   Serial.begin(9600);
+  // Connect Wifi
   client.wifiConnection(SSID,PASS);
+  // Open TCP connection
   client.tcpConnection();
 }
-
 
 void loop() {
   float value = analogRead(A0);
   float value2 = analogRead(A1);
   float value3 = analogRead(A2);
   float value4 = analogRead(A3);
+  // add send values
   client.add(ID,value,"","");
   client.add(ID2,value2,"","");
   client.add(ID3,value3,"","");
   client.add(ID4,value4,"","");
+  // Send multipule value
   client.sendAll();
 }
